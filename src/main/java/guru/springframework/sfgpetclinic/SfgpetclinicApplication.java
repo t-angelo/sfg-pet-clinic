d115e1b8e9f7adaf1030552bcd466c9b498784d8
625610 <tsaganeas_angelo@bah.com>
2022-08-12 08:56:20 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/sfgpetclinic/SfgpetclinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgpetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgpetclinicApplication.class, args);
+	}
+
+}
